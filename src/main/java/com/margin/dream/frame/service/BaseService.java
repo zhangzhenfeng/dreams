76a28708c6c79de2c4e71dad82ca18168e1a6b4e
zhangzhenfeng <990768144@qq.com>
2014-10-29 20:52:05 +0800

    提交BaseService

---
+++ src/main/java/com/margin/dream/frame/service/BaseService.java
@@ -0,0 +1,5 @@
+package com.margin.dream.frame.service;
+
+public class BaseService {
+
+}
---
+++ src/main/java/com/margin/dream/frame/util/WebJsonUtil.java
@@ -0,0 +1,50 @@
+package com.margin.dream.frame.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Copyright(C) JiNanShangJie 2014.
+ * 
+ * Json转换类.
+ * 
+ * @author 张孝党 2014/04/18.
+ * 
+ * @version V1.00.
+ * 
+ *          更新履历： V1.00 2013/04/18 张孝党 创建.
+ */
+public class WebJsonUtil {
+
+	/**
+	 * 把对象转换为json字符串
+	 * 
+	 * @param obj
+	 *            要转换的对象
+	 * @return json字符串
+	 */
+	public static String bean2Json(Object obj) {
+
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		// 返回
+		return gson.toJson(obj);
+	}
+
+	/**
+	 * 把json字符串转换为对象.
+	 * 
+	 * @param jsonStr
+	 *            json字符串.
+	 * 
+	 * @param objClass
+	 *            对象类.
+	 * 
+	 * @return 对象
+	 */
+	public static <T> T json2Bean(String json, Class<T> classOfT) {
+
+		Gson gson = new GsonBuilder().create();
+		// 返回
+		return gson.fromJson(json, classOfT);
+	}
+}
