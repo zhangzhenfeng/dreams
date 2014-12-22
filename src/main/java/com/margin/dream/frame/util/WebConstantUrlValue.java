98ce4a8b420fe7ef8b602ba5437f3099daf03c62
zhangzhenfeng <990768144@qq.com>
2014-12-22 20:54:57 +0800

    提交WebConstantUrlValue.java

---
+++ src/main/java/com/margin/dream/frame/util/WebConstantUrlValue.java
@@ -0,0 +1,18 @@
+package com.margin.dream.frame.util;
+
+/**
+ * Copyright(C) JiNanShangJie 2014.
+ * 
+ * 项目所有的请求URL
+ * 
+ * @author 张振峰 2014/11/01.
+ * 
+ * @version V1.00.
+ * 
+ *          更新履历： V1.00 2014/11/01 张振峰 创建.
+ */
+public class WebConstantUrlValue {
+	
+	/** 登陆的URL **/
+	public static final String LOGIN = "/login";
+}
